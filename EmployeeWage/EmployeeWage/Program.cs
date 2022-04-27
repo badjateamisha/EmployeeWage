eec2df465bd09678413b2c69645d9bfd2b091d99
badjateamisha <amishabadjate175@gmail.com>
2022-04-27 10:42:18 +0530

    Classes and Objects

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeWage;
+
+Console.WriteLine("Welcome to Employee Wage computation program!");
+
+Finalwage wage = new();
+wage.finalwage();
