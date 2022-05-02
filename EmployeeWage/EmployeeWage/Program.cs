129168fce614deb1ed4cb74982a2317956c62c51
badjateamisha <amishabadjate175@gmail.com>
2022-05-02 14:18:00 +0530

    Multiple Companies

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -1,7 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using EmployeeWage;
 
-Console.WriteLine("Welcome to Employee Wage computation program!");
 
-Finalwage wage = new();
-wage.finalwage();
+public class wageforeachcompany
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to employee wage program!");
+        while (true)
+        {
+            Console.WriteLine("Choose the program to be executed:\n1)Wage for multiple companies\n2)total wage for each company\n3)Different Empwages for different companies");
+
+
+
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 8:
+                    WageCalculation wage = new WageCalculation();
+
+                    wage.EmployeeWage("BridgeLabs", 20, 20, 100);
+                    wage.EmployeeWage("Amazon", 30, 20, 120);
+
+                    break;
+                case 9:
+
+                    wageCalculation Amazon = new wageCalculation("Amazon", 20, 5, 100);
+                    Amazon.computewage();
+                    wageCalculation BridgeLabs = new wageCalculation("Bridge Labs", 25, 5, 120);
+                    BridgeLabs.computewage();
+                    break;
+                case 10:
+                    EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+                    empWageBuilder.addCompanyEmpWage("Amazon", 20, 5, 100);
+                    empWageBuilder.addCompanyEmpWage("Bridge Labs", 25, 5, 120);
+                    empWageBuilder.ComputeEmpWage();
+                    Console.Write("Total wage for company Amazon :");
+                    empWageBuilder.getTotalWage("Amazon");
+                    Console.WriteLine("\n");
+                    break;
+
+
+            }
+        }
+    }
+}
